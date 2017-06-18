ea6d2c7b22f471f511818b2a2f8ba2bdc65dd979
Shikha Mehta <s.mehta@ppc-ag.de>
2017-06-18 23:33:24 +0200

    small changes

--- ibench.py
+++ ibench.py
@@ -3,9 +3,8 @@ import sys,os
 
 # shell mode
 def shell_mode():
-    #TBD: Later add parsing of shell args
-    pc_proj_path = '/export/home/smehta/ilastik/projects/mitocheck_2d+t/pixelClassification.ilp'
-    pc_data_files = ['/export/home/smehta/miniconda2/envs/project/data/iso.01500.png', '/export/home/smehta/miniconda2/envs/project/data/iso.01501.png']
+    pc_proj_path = './2d_project_pc_all_features.ilp'
+    pc_data_files = ['./data/iso.01500.png', './data/iso.01501.png']
     test_list = [PixelClassificationBM(pc_proj_path,pc_data_files)]
     [test.run() for test in test_list]
 
